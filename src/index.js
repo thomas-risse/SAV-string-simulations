88dcc1397828143a40c501d7fdf6b66d47062586
thomas-risse <96129668+thomas-risse@users.noreply.github.com>
2025-02-03 13:52:23 +0000

    Deploying to gh-pages from @ thomas-risse/SAV-string-simulations@62bdd74911e0f3d6a1848b1a14a581c3e3966bd9 🚀

--- src/index.js
+++ src/index.js
@@ -10,11 +10,20 @@ await audioContext.audioWorklet.addModule('./src/StringProcessor.js');
 
 let impulseWidth = 1 / 1000;
 
-const string = new AudioWorkletNode(audioContext, 'string-processor');
+// Parameters
+let options = {};
+options.processorOptions = {};
+options.processorOptions.t60_0 = 12;
+options.processorOptions.t60_1000 = 8;
+options.processorOptions.f0 = 150;
+options.processorOptions.beta = 0.001;
+
+let stringNode = new AudioWorkletNode(audioContext, 'string-processor', options);
 const inGainNode = new GainNode(audioContext);
 inGainNode.gain.value = 0.8;
 const outGainNode = new GainNode(audioContext);
-inGainNode.connect(string).connect(outGainNode).connect(audioContext.destination);
+inGainNode.connect(stringNode).connect(outGainNode).connect(audioContext.destination);
+
 
 function struckSource() {
   const N = impulseWidth * audioContext.sampleRate;
@@ -42,6 +51,9 @@ function pluckSource() {
   bufferSource.start();
 }
 
+function updateParameters() {
+  stringNode.port.postMessage(options);
+}
 
 render(html`
   <h1>Interactive simulation</h1>
@@ -49,6 +61,45 @@ render(html`
     <sc-icon type="github" href="https://github.com/thomas-risse/SAV-string-simulations"></sc-icon>
   </div>
   <div>
+  <div style="padding: 4px 0;"></div>
+      <sc-button @click=${updateParameters}>Update parameters </sc-button>
+      <sc-text> Fundamental frequency </sc-text>
+      <sc-number
+        min = 20
+        max = 10000
+        value=${options.processorOptions.f0}
+        @input=${function(e){
+          options.processorOptions.f0 = e.detail.value;
+        }}
+      ></sc-number> 
+      <sc-text> Inharmonicity </sc-text>
+      <sc-number
+        min = 1e-5
+        max = 1
+        value=${options.processorOptions.beta}
+        @input=${function(e){
+          options.processorOptions.beta = e.detail.value;
+        }}
+      ></sc-number> 
+      <sc-text> Decay time 0 Hz </sc-text>
+      <sc-number
+        min = 0.01
+        max = 1e10
+        value=${options.processorOptions.t60_0}
+        @input=${function(e){
+          options.processorOptions.t60_0 = e.detail.value;
+        }}
+      ></sc-number> 
+      <sc-text> Decay time 1000 Hz </sc-text>
+      <sc-number
+        min = 0.01
+        max = 1e10
+        value=${options.processorOptions.t60_1000}
+        @input=${function(e){
+          options.processorOptions.t60_1000 = e.detail.value;
+        }}
+      ></sc-number> 
+    </div>
     <div style="padding: 4px 0;"></div>
       <sc-button @click=${struckSource}>Impulse (struck) </sc-button>
       <sc-button @click=${pluckSource}>Impulse (pluck) </sc-button>
