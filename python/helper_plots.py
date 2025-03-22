58cadea49beedaa178065cccbdd53c53a99b8f5d
thomas-risse <risse@ircam.fr>
2025-03-22 12:34:29 +0100

    Change plot legend size

--- python/helper_plots.py
+++ python/helper_plots.py
@@ -10,8 +10,8 @@ tex_fonts = {
     "text.usetex": True,
     "font.family": "serif",
     # Use 10pt font in plots, to match 10pt font in document
-    "axes.labelsize": 10,
-    "font.size": 10,
+    "axes.labelsize": 8,
+    "font.size": 8,
     # Make the legend/label fonts a little smaller
     "legend.fontsize": 8,
     "xtick.labelsize": 8,
